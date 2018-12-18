1c8c024cc40156ee8a9ec1dc9c8b535ce6d611bb
Overzet <overzet@gmail.com>
2018-12-18 13:36:31 +0100

    "Initialization"

---
+++ src/main/java/be/intecbrussel/mountainclient/MountainClientApplication.java
@@ -0,0 +1,14 @@
+package be.intecbrussel.mountainclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountainClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MountainClientApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/be/intecbrussel/mountainclient/MountainClientApplicationTests.java
@@ -0,0 +1,17 @@
+package be.intecbrussel.mountainclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MountainClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
